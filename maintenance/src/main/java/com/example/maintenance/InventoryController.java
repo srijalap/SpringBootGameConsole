5702d724e8b5e6b6379f64e2c7705381711a12d8
Srijala Poudel <srijana.poudel@tuni.fi>
2023-11-18 15:54:40 +0200

    Initial commit

---
+++ maintenance/src/main/java/com/example/maintenance/InventoryController.java
@@ -0,0 +1,11 @@
+package com.example.maintenance;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryController {
+    @GetMapping("/info")
+    public String infoService(){
+        return "This is my Drone and I am happy now. All is well.";
+    }
+}
